f45a172da96283cf9a70a09a31f8df67109d8bbf
Ibraheem Z. Abu-Kaff <eng.ibraheemabukaff@gmail.com>
2019-11-08 14:44:19 +0400

    fix(AuthMiddleware): Fixing the middle ware for the Authrization/Authentication

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,9 +2,14 @@ import jwt from "jsonwebtoken";
 import User from "../models/User";
 
 const Auth = async (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    const data = jwt.verify(token, process.env.JWT_KEY);
+    const authorizationHeaderValue = req.header('Authorization') || '';
+    if (!authorizationHeaderValue) {
+        res.status(401).send({error: 'Not authorized to access this resource'})
+    }
+
+    const token = authorizationHeaderValue.replace('Bearer ', '');
     try {
+        const data = jwt.verify(token, process.env.JWT_KEY);
         const user = await User.findOne({_id: data._id, 'tokens.token': token});
         if (!user) {
             throw new Error();
