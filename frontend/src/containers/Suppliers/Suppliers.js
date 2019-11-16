9f31d43a414b216c48e53c988e318084447954a3
Ibraheem Z. Abu-Kaff <eng.ibraheemabukaff@gmail.com>
2019-11-16 21:16:32 +0400

    feat(supplier): Logout button

--- frontend/src/containers/Suppliers/Suppliers.js
+++ frontend/src/containers/Suppliers/Suppliers.js
@@ -1,12 +1,26 @@
 import React, {Component} from "react";
 //import "./Login.css";
+import ButtonElement from "../../components/ButtonElement";
+import {doLogout} from "../../helpers";
 
 class Suppliers extends Component {
+
+    constructor(props) {
+        super(props);
+        this.onLogout = this.onLogout.bind(this);
+    }
+
+    onLogout = (eve) => {
+        eve.preventDefault();
+        eve.stopPropagation();
+        doLogout();
+    };
 
     render() {
         return (
             <div>
                 Supplier
+                <h4><ButtonElement onClick={this.onLogout} id="logout" type="submit" label="Logout"/></h4>
             </div>
 
         );
